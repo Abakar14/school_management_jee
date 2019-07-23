58e8b40a8826c341b96f399dc6c2afe6dd93fa91
abakar14 <abakar61@web.de>
2019-07-23 18:35:13 +0200

    added new address, country, state

--- database/datamodel/create_user_role_permission_table.sql
+++ database/datamodel/create_user_role_permission_table.sql
@@ -36,6 +36,71 @@ COMMENT = 'The role table contain all roles of the System (Rolle | funktion)';
 SHOW WARNINGS;
 
 -- -----------------------------------------------------
+-- Table `country`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `country` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `country` (
+  `country_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `abbreviations` VARCHAR(15) NULL COMMENT 'Länderabkurzungen (DE, FR)',
+  PRIMARY KEY (`country_id`))
+ENGINE = InnoDB;
+
+SHOW WARNINGS;
+
+-- -----------------------------------------------------
+-- Table `state`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `state` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `state` (
+  `state_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `substate_name` VARCHAR(45) NULL,
+  `country_id_fk` INT NOT NULL,
+  PRIMARY KEY (`state_id`),
+  CONSTRAINT `country_id_fk`
+    FOREIGN KEY (`country_id_fk`)
+    REFERENCES `country` (`country_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+SHOW WARNINGS;
+CREATE INDEX `country_id_fk_idx` ON `state` (`country_id_fk` ASC);
+
+SHOW WARNINGS;
+
+-- -----------------------------------------------------
+-- Table `address`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `address` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `address` (
+  `address_id` INT NOT NULL AUTO_INCREMENT,
+  `city` VARCHAR(45) NULL,
+  `street` VARCHAR(45) NULL,
+  `postcode` VARCHAR(45) NULL,
+  `number` VARCHAR(45) NULL,
+  `state_id_fk` INT NULL,
+  PRIMARY KEY (`address_id`),
+  CONSTRAINT `state_id_fk`
+    FOREIGN KEY (`state_id_fk`)
+    REFERENCES `state` (`state_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+SHOW WARNINGS;
+CREATE INDEX `state_id_fk_idx` ON `address` (`state_id_fk` ASC);
+
+SHOW WARNINGS;
+
+-- -----------------------------------------------------
 -- Table `user`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `user` ;
@@ -45,9 +110,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
-  `password` varchar(100) COLLATE utf32_bin NOT NULL,
-  `birthday` date DEFAULT NULL,
-  PRIMARY KEY (`user_id`))
+  `address_id_fk` INT NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `birthday` DATE NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `address_id_fk`
+    FOREIGN KEY (`address_id_fk`)
+    REFERENCES `address` (`address_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf32
 COLLATE = utf32_bin
@@ -55,6 +126,9 @@ COMMENT = 'Table of all users in our user management system';
 
 SHOW WARNINGS;
 CREATE UNIQUE INDEX `user_id_UNIQUE` ON `user` (`user_id` ASC);
+
+SHOW WARNINGS;
+CREATE INDEX `address_id_fk_idx` ON `user` (`address_id_fk` ASC);
 
 SHOW WARNINGS;
 
